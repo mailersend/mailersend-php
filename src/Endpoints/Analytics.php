ac62bdaf2e5803593701ebb5439a56e758758478
João Cruz <joao.fs.cruz97@gmail.com>
2021-06-09 10:44:13 +0100

    Implement opens endpoints

--- src/Endpoints/Analytics.php
+++ src/Endpoints/Analytics.php
@@ -4,6 +4,7 @@ namespace MailerSend\Endpoints;
 
 use Assert\Assertion;
 use MailerSend\Helpers\Builder\ActivityAnalyticsParams;
+use MailerSend\Helpers\Builder\OpensAnalyticsParams;
 use MailerSend\Helpers\GeneralHelpers;
 
 class Analytics extends AbstractEndpoint
@@ -43,6 +44,36 @@ class Analytics extends AbstractEndpoint
 
         return $this->httpLayer->get(
             $this->buildUri('analytics/date', $activityAnalyticsParams->toArray())
+        );
+    }
+
+    public function opensByCountry(OpensAnalyticsParams $opensAnalyticsParams): array
+    {
+        return $this->callOpensEndpoint('analytics/country', $opensAnalyticsParams);
+    }
+
+    public function opensByUserAgentName(OpensAnalyticsParams $opensAnalyticsParams): array
+    {
+        return $this->callOpensEndpoint('analytics/ua-name', $opensAnalyticsParams);
+    }
+
+    public function opensByReadingEnvironment(OpensAnalyticsParams $opensAnalyticsParams): array
+    {
+        return $this->callOpensEndpoint('analytics/ua-type', $opensAnalyticsParams);
+    }
+
+    protected function callOpensEndpoint(string $path, OpensAnalyticsParams $opensAnalyticsParams): array
+    {
+        GeneralHelpers::assert(
+            fn () => Assertion::greaterThan(
+                $opensAnalyticsParams->getDateTo(),
+                $opensAnalyticsParams->getDateFrom(),
+                'The parameter date_to must be greater than date_from.'
+            )
+        );
+
+        return $this->httpLayer->get(
+            $this->buildUri($path, $opensAnalyticsParams->toArray())
         );
     }
 }
