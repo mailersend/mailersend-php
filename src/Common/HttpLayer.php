d135abe7f345fe5cf0166b0d1569f13fe2707ba2
Abdelilah <lbardi.abdelilah@gmail.com>
2021-06-10 15:41:04 +0100

    Adds support for get in httplayer

--- src/Common/HttpLayer.php
+++ src/Common/HttpLayer.php
@@ -94,6 +94,18 @@ class HttpLayer
      * @throws JsonException
      * @throws ClientExceptionInterface
      */
+    public function get(string $uri, array $body): array
+    {
+        $request = $this->requestFactory->createRequest('GET', $uri)
+            ->withBody($this->buildBody($body));
+
+        return $this->buildResponse($this->pluginClient->sendRequest($request));
+    }
+
+    /**
+     * @throws JsonException
+     * @throws ClientExceptionInterface
+     */
     public function request(string $method, string $uri, string $body = ''): array
     {
         $request = $this->requestFactory->createRequest($method, $uri);
