99605bd2a441d81fa748a666b00f1218b328b8c0
João Cruz <joao.fs.cruz97@gmail.com>
2021-06-11 15:13:20 +0100

    Remove DomainParams

--- src/Endpoints/Domain.php
+++ src/Endpoints/Domain.php
@@ -3,50 +3,70 @@
 namespace MailerSend\Endpoints;
 
 use Assert\Assertion;
-use MailerSend\Helpers\Builder\DomainParams;
+use MailerSend\Common\Constants;
+use MailerSend\Helpers\Builder\DomainSettingsParams;
 use MailerSend\Helpers\GeneralHelpers;
 
 class Domain extends AbstractEndpoint
 {
-    protected string $base_endpoint = 'domains';
+    protected string $endpoint = 'domains';
 
     /**
      * @throws \Psr\Http\Client\ClientExceptionInterface
      * @throws \MailerSend\Exceptions\MailerSendAssertException
      * @throws \JsonException
      */
-    public function domainList(DomainParams $domainParams): array
+    public function domainList(?int $page = null, ?int $limit = Constants::DEFAULT_LIMIT, ?bool $verified = null): array
     {
-        if ($domainParams->getLimit()) {
+        if ($limit) {
             GeneralHelpers::assert(
-                fn () => Assertion::range($domainParams->getLimit(), 10, 100, 'Limit is supposed to be between 10 and 100.')
+                fn () => Assertion::range(
+                    $limit,
+                    Constants::MIN_LIMIT,
+                    Constants::MAX_LIMIT,
+                    'Limit is supposed to be between ' . Constants::MIN_LIMIT . ' and ' . Constants::MAX_LIMIT .  '.'
+                )
             );
         }
 
         return $this->httpLayer->get(
-            $this->buildUri($this->base_endpoint, $domainParams->toArray())
+            $this->buildUri($this->endpoint, [
+                'page' => $page,
+                'limit' => $limit,
+                'verified' => $verified,
+            ])
         );
     }
 
     /**
      * @throws \Psr\Http\Client\ClientExceptionInterface
      * @throws \JsonException
+     * @throws \MailerSend\Exceptions\MailerSendAssertException
      */
     public function domain(string $domainId): array
     {
+        GeneralHelpers::assert(
+            fn () => Assertion::minLength($domainId, 1, 'Domain id is required.')
+        );
+
         return $this->httpLayer->get(
-            $this->buildUri($this->base_endpoint . "/$domainId")
+            $this->buildUri("$this->endpoint/$domainId")
         );
     }
 
     /**
      * @throws \Psr\Http\Client\ClientExceptionInterface
      * @throws \JsonException
+     * @throws \MailerSend\Exceptions\MailerSendAssertException
      */
     public function deleteDomain(string $domainId): array
     {
+        GeneralHelpers::assert(
+            fn () => Assertion::minLength($domainId, 1, 'Domain id is required.')
+        );
+
         return $this->httpLayer->delete(
-            $this->buildUri($this->base_endpoint . "/$domainId")
+            $this->buildUri("$this->endpoint/$domainId")
         );
     }
 
@@ -55,16 +75,31 @@ class Domain extends AbstractEndpoint
      * @throws \MailerSend\Exceptions\MailerSendAssertException
      * @throws \JsonException
      */
-    public function recipients(string $domainId, DomainParams $domainParams): array
+    public function recipients(string $domainId, ?int $page = null, ?int $limit = Constants::DEFAULT_LIMIT): array
     {
-        if ($domainParams->getLimit()) {
+        GeneralHelpers::assert(
+            fn () => Assertion::minLength($domainId, 1, 'Domain id is required.')
+        );
+
+        if ($limit) {
             GeneralHelpers::assert(
-                fn () => Assertion::range($domainParams->getLimit(), 10, 100, 'Limit is supposed to be between 10 and 100.')
+                fn () => Assertion::range(
+                    $limit,
+                    Constants::MIN_LIMIT,
+                    Constants::MAX_LIMIT,
+                    'Limit is supposed to be between ' . Constants::MIN_LIMIT . ' and ' . Constants::MAX_LIMIT .  '.'
+                )
             );
         }
 
         return $this->httpLayer->get(
-            $this->buildUri($this->base_endpoint . "/$domainId/recipients", $domainParams->toArray())
+            $this->buildUri("$this->endpoint/$domainId/recipients", [
+                'page' => $page,
+                'limit' => $limit,
+            ])
+        );
+    }
+
         );
     }
 }
