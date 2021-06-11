f45b5335018975ad0e58beb9d8366edaf299d760
João Cruz <joao.fs.cruz97@gmail.com>
2021-06-11 09:46:07 +0100

    Add endpoint to MailerSend class

--- src/MailerSend.php
+++ src/MailerSend.php
@@ -3,6 +3,7 @@
 namespace MailerSend;
 
 use MailerSend\Common\HttpLayer;
+use MailerSend\Endpoints\Activity;
 use MailerSend\Endpoints\Email;
 use MailerSend\Endpoints\Token;
 use MailerSend\Exceptions\MailerSendException;
@@ -29,6 +30,7 @@ class MailerSend
 
     public Email $email;
     public Token $token;
+    public Activity $activity;
 
     /**
      * @param  array  $options  Additional options for the SDK
@@ -46,6 +48,7 @@ class MailerSend
     {
         $this->email = new Email($this->httpLayer, $this->options);
         $this->token = new Token($this->httpLayer, $this->options);
+        $this->activity = new Activity($this->httpLayer, $this->options);
     }
 
     protected function setHttpLayer(?HttpLayer $httpLayer = null): void
