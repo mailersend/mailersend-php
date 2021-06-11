e6c72f6b0f930d3a352cd396e21c225f2d836c20
Abdelilah <lbardi.abdelilah@gmail.com>
2021-06-11 09:58:26 +0100

    Adds support for PUT methods

--- src/Common/HttpLayer.php
+++ src/Common/HttpLayer.php
@@ -94,6 +94,18 @@ class HttpLayer
      * @throws JsonException
      * @throws ClientExceptionInterface
      */
+    public function put(string $uri, array $body): array
+    {
+        $request = $this->requestFactory->createRequest('PUT', $uri)
+            ->withBody($this->buildBody($body));
+
+        return $this->buildResponse($this->pluginClient->sendRequest($request));
+    }
+
+    /**
+     * @throws JsonException
+     * @throws ClientExceptionInterface
+     */
     public function get(string $uri, array $body): array
     {
         $request = $this->requestFactory->createRequest('GET', $uri)
