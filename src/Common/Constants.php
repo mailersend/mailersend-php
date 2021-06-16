12fc0077c623baafa8dbcb410190ee39f5bd18b0
Tautvydas <tautvydas@mailerlite.com>
2021-06-16 12:51:19 +0300

    version constant update

--- src/Common/Constants.php
+++ src/Common/Constants.php
@@ -4,7 +4,7 @@ namespace MailerSend\Common;
 
 class Constants
 {
-    public const SDK_VERSION = 'v0.1';
+    public const SDK_VERSION = 'v0.3.1';
     public const DEFAULT_LIMIT = 25;
     public const MIN_LIMIT = 10;
     public const MAX_LIMIT = 100;
