ff2a2a0720dccf344ed5ab709a8cdb64c2fe8477
João Cruz <joao.fs.cruz97@gmail.com>
2021-06-16 09:27:50 +0100

    Remove unnecessary methods

--- src/Common/HttpLayer.php
+++ src/Common/HttpLayer.php
@@ -91,29 +91,6 @@ class HttpLayer
     }
 
     /**
-     * @throws ClientExceptionInterface
-     * @throws JsonException
-     */
-    public function put(string $uri, array $body): array
-    {
-        $request = $this->requestFactory->createRequest('PUT', $uri)
-            ->withBody($this->buildBody($body));
-
-        return $this->buildResponse($this->pluginClient->sendRequest($request));
-    }
-
-    /**
-     * @throws ClientExceptionInterface
-     * @throws JsonException
-     */
-    public function delete(string $uri): array
-    {
-        $request = $this->requestFactory->createRequest('DELETE', $uri);
-
-        return $this->buildResponse($this->pluginClient->sendRequest($request));
-    }
-
-    /**
      * @throws JsonException
      * @throws ClientExceptionInterface
      */
