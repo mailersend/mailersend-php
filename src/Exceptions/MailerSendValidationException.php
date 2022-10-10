7b97814482b88eba998e621a1c0f7d87a453b70d
Ramunas Pabreza <doobas@gmail.com>
2022-10-10 13:43:08 +0300

    Fixed stream issue, added easy validation data access

--- src/Exceptions/MailerSendValidationException.php
+++ src/Exceptions/MailerSendValidationException.php
@@ -9,13 +9,23 @@ class MailerSendValidationException extends MailerSendException
 {
     protected RequestInterface $request;
     protected ResponseInterface $response;
+    protected string $body;
+    protected int $statusCode;
+    protected array $errors;
 
     public function __construct(
         RequestInterface $request,
         ResponseInterface $response
     ) {
-        $body = $response->getBody()->getContents();
-        $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+        $this->body = $response->getBody()->getContents();
+        //Rewind stream to beginning of the file to use it for the second time
+        $response->getBody()->rewind();
+
+        $this->statusCode = $response->getStatusCode();
+
+        $data = json_decode($this->body, true, 512, JSON_THROW_ON_ERROR);
+
+        $this->errors = $data['errors'] ?? [];
 
         parent::__construct($data['message']);
 
@@ -31,5 +41,20 @@ class MailerSendValidationException extends MailerSendException
     public function getRequest(): RequestInterface
     {
         return $this->request;
+    }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
     }
 }
